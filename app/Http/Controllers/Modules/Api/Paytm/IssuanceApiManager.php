23f8eae259c7bd12b61d008f45c324a70fbe88ac
BHASKAR SHUKLA <bhaskarshukla274@gmail.com>
2023-10-04 21:25:30 +0530

    Change Log
    1.Paytm Issuance Api sendOtp error Handling

--- app/Http/Controllers/Modules/Api/Paytm/IssuanceApiManager.php
+++ app/Http/Controllers/Modules/Api/Paytm/IssuanceApiManager.php
@@ -4,27 +4,36 @@ namespace App\Http\Controllers\Modules\Api\Paytm;
 
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Mockery\Exception;
 
 class IssuanceApiManager extends Controller
 {
     // ----------------------------------------- DEBUG -----------------------------------------
-
-
-    public function sendOtp(Request $request): Collection
+    public function sendOtp(Request $request): string
     {
         $body = $request->input('body');
         $token = $request->input('header')['authorization'];
-        return Http::withHeaders([
-            'Authorization' => $token
-        ])
-            ->timeout(3*60)->retry(3)
-            ->withBody(json_encode($body), 'application/json')
-            ->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/otp/send")
-            ->collect();
+
+        try {
+            $client = new Client();
+            $response = $client->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/otp/send", [
+                'headers' => [
+                    'Authorization' => $token,
+                    'Content-Type' => 'application/json',
+                ],
+                'body' => json_encode($body),
+                'timeout' => 3 * 60,
+                'http_errors' => false,
+            ]);
+            return json_decode($response->getBody()->getContents());
+        } catch (Exception|GuzzleException $e) {
+            return $e->getMessage();
+        }
     }
 
     function verifyOtp(Request $request): Collection
@@ -34,7 +43,7 @@ class IssuanceApiManager extends Controller
         return Http::withHeaders([
             'Authorization' => $token
         ])
-            ->timeout(3*60)->retry(3)
+            ->timeout(3 * 60)->retry(3)
             ->withBody(json_encode($body), 'application/json')
             ->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/otp/verify")
             ->collect();
@@ -61,7 +70,7 @@ class IssuanceApiManager extends Controller
             'Authorization' => $token,
             'sessionId' => $session
         ])
-            ->timeout(3*60)->retry(3)
+            ->timeout(3 * 60)->retry(3)
             ->withBody($body, '')
             ->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/kyc/submit")
             ->collect();
@@ -76,7 +85,7 @@ class IssuanceApiManager extends Controller
             'Authorization' => $token,
             'sessionId' => $session
         ])
-            ->timeout(3*60)->retry(3)
+            ->timeout(3 * 60)->retry(3)
             ->withBody(json_encode($body), 'application/json')
             ->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/card/activation")
             ->collect();
