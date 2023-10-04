f6cfb216a9e42f37ad35048b90e1f18cfbb10d36
BHASKAR SHUKLA <bhaskarshukla274@gmail.com>
2023-10-04 22:08:29 +0530

    Change Log
    1.Paytm Issuance Api sendOtp error Handling for all debug function

--- app/Http/Controllers/Modules/Api/Paytm/IssuanceApiManager.php
+++ app/Http/Controllers/Modules/Api/Paytm/IssuanceApiManager.php
@@ -6,9 +6,6 @@ use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Mockery\Exception;
 
 class IssuanceApiManager extends Controller
@@ -41,76 +38,146 @@ class IssuanceApiManager extends Controller
         }
     }
 
-    function verifyOtp(Request $request): Collection
+    public function verifyOtp(Request $request)
     {
         $body = $request->input('body');
         $token = $request->input('header')['authorization'];
-        return Http::withHeaders([
-            'Authorization' => $token
-        ])
-            ->timeout(3 * 60)->retry(3)
-            ->withBody(json_encode($body), 'application/json')
-            ->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/otp/verify")
-            ->collect();
+
+        try {
+            $client = new Client();
+            $response = $client->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/otp/verify", [
+                'headers' => [
+                    'Authorization' => $token,
+                    'Content-Type' => 'application/json',
+                ],
+                'body' => json_encode($body),
+                'timeout' => 3 * 60,
+                'http_errors' => false,
+            ]);
+            return json_decode($response->getBody()->getContents());
+        } catch (Exception | GuzzleException $e) {
+            return \Response::json([
+                'body' => [
+                    'resultCode' => 101,
+                    'resultMsg' => $e->getMessage()
+                ]
+            ]);
+        }
     }
 
-    function kycDetail(Request $request): Collection
+    public function kycDetail(Request $request)
     {
         $token = $request->input('header')['authorization'];
         $session = $request->input('header')['sessionId'];
-        return Http::withHeaders([
-            'Authorization' => $token,
-            'sessionId' => $session
-        ])
-            ->get("https://tapcard-issuer-stage.paytm.com/middleware/v1/kyc/limit/details")
-            ->collect();
-    }
 
-    function submitKyc(Request $request): Collection
+        try {
+            $client = new Client();
+            $response = $client->get("https://tapcard-issuer-stage.paytm.com/middleware/v1/kyc/limit/details", [
+                'headers' => [
+                    'Authorization' => $token,
+                    'sessionId' => $session,
+                    'Content-Type' => 'application/json',
+                ],
+                'timeout' => 3 * 60,
+                'http_errors' => false,
+            ]);
+            return json_decode($response->getBody()->getContents());
+        } catch (Exception | GuzzleException $e) {
+            return \Response::json([
+                'body' => [
+                    'resultCode' => 101,
+                    'resultMsg' => $e->getMessage()
+                ]
+            ]);
+        }
+    }
+    public function submitKyc(Request $request)
     {
         $body = $request->input('body');
         $token = $request->input('header')['authorization'];
         $session = $request->input('header')['sessionId'];
-        return Http::withHeaders([
-            'Authorization' => $token,
-            'sessionId' => $session
-        ])
-            ->timeout(3 * 60)->retry(3)
-            ->withBody($body, '')
-            ->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/kyc/submit")
-            ->collect();
-    }
 
-    function activateCard(Request $request): Collection
+        try {
+            $client = new Client();
+            $response = $client->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/kyc/submit", [
+                'headers' => [
+                    'Authorization' => $token,
+                    'sessionId' => $session,
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => $body, // Use 'json' to send JSON data directly
+                'timeout' => 3 * 60,
+                'http_errors' => false,
+            ]);
+            return json_decode($response->getBody()->getContents());
+        } catch (Exception | GuzzleException $e) {
+            return \Response::json([
+                'body' => [
+                    'resultCode' => 101,
+                    'resultMsg' => $e->getMessage()
+                ]
+            ]);
+        }
+    }
+    public function activateCard(Request $request)
     {
         $body = $request->input('body');
         $token = $request->input('header')['authorization'];
         $session = $request->input('header')['sessionId'];
-        return Http::withHeaders([
-            'Authorization' => $token,
-            'sessionId' => $session
-        ])
-            ->timeout(3 * 60)->retry(3)
-            ->withBody(json_encode($body), 'application/json')
-            ->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/card/activation")
-            ->collect();
+
+        try {
+            $client = new Client();
+            $response = $client->post("https://tapcard-issuer-stage.paytm.com/middleware/v1/card/activation", [
+                'headers' => [
+                    'Authorization' => $token,
+                    'sessionId' => $session,
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => $body, // Use 'json' to send JSON data directly
+                'timeout' => 3 * 60,
+                'http_errors' => false,
+            ]);
+            return json_decode($response->getBody()->getContents());
+        } catch (Exception | GuzzleException $e) {
+            return \Response::json([
+                'body' => [
+                    'resultCode' => 101,
+                    'resultMsg' => $e->getMessage()
+                ]
+            ]);
+        }
     }
 
-    function activationStatus(Request $request): Collection
+    public function activationStatus(Request $request)
     {
         $token = $request->input('header')['authorization'];
         $inputType = $request->input('query')['inputType'];
         $data = $request->input('query')['data'];
-        return Http::withHeaders([
-            'Authorization' => $token
-        ])
-            ->get("https://tapcard-issuer-stage.paytm.com/middleware/v2/card/poll/status", [
-                'inputType' => $inputType,
-                'data' => $data
-            ])
-            ->collect();
-    }
 
+        try {
+            $client = new Client();
+            $response = $client->get("https://tapcard-issuer-stage.paytm.com/middleware/v2/card/poll/status", [
+                'headers' => [
+                    'Authorization' => $token,
+                    'Content-Type' => 'application/json',
+                ],
+                'query' => [
+                    'inputType' => $inputType,
+                    'data' => $data,
+                ],
+                'timeout' => 3 * 60,
+                'http_errors' => false,
+            ]);
+            return json_decode($response->getBody()->getContents());
+        } catch (Exception | GuzzleException $e) {
+            return \Response::json([
+                'body' => [
+                    'resultCode' => 101,
+                    'resultMsg' => $e->getMessage()
+                ]
+            ]);
+        }
+    }
 
 
     // -------------------------------------------- PROD -----------------------------------------
