c44946b6303968cb174389376b44e04348a007aa
shuklabhaskar <bhaskarshukla274@gmail.com>
2024-10-21 23:32:00 +0530

    # Release Log - v4.1.3
    ---
    ## API Update
    ---
    ## ClAccounting.php Api has implemented with GRA 66

--- app/Http/Controllers/Modules/Api/CL/ClAccounting.php
+++ app/Http/Controllers/Modules/Api/CL/ClAccounting.php
@@ -36,6 +36,7 @@ class ClAccounting extends Controller
         $transactions = json_decode($request->getContent(), true);
         $response     = [];
 
+
         if ($transactions == [] || $transactions == null) {
 
             return response([
@@ -119,25 +120,35 @@ class ClAccounting extends Controller
 
             } elseif ($opTypeId == 64) {
 
-                $transData  = $this->exitMismatchNoExit($transaction);
+                /** Entry Mismatch - Other Time (No Exit) **/
+                $transData  = $this->entryMismatchOtherTime($transaction);
                 $response[] = $transData;
 
             } elseif ($opTypeId == 65) {
 
+                /** Exit Mismatch - Same Time **/
+                $transData  = $this->exitMismatchNoExit($transaction);
+                $response[] = $transData;
+
+            } elseif ($opTypeId == 66) {
+
+                /** Exit Mismatch - Other Time (No Entry) **/
                 $transData  = $this->exitMismatchNoEntry($transaction);
                 $response[] = $transData;
 
             } else {
-                $transData['is_settled'] = false;
-                $transData['atek_id'] = $transaction['atek_id'];
-                $transData['error'] = "Invalid Product Type ID !";
+
+                $transData['is_settled']    = false;
+                $transData['atek_id']       = $transaction['atek_id'];
+                $transData['error']         = "Invalid Product Type ID !";
+
             }
 
         }
 
         return response([
             'status' => true,
-            'trans' => $response
+            'trans'  => $response
         ]);
 
     }
@@ -2094,8 +2105,7 @@ class ClAccounting extends Controller
         return $transData;
     }/* OP_TYPE_ID = 63*/
 
-    public function exitMismatchNoExit($transaction)
-    {
+    public function entryMismatchOtherTime($transaction){
 
         /* CHECK THAT IS THESE ATTRIBUTES ARE NULLABLE OR NOT */
         $paxFirstName = "";
@@ -2118,7 +2128,7 @@ class ClAccounting extends Controller
                     'atek_id' => $transaction['atek_id'],
                     'txn_date' => $transaction['txn_date'],
                     'engraved_id' => $transaction['engraved_id'],
-                    'op_type_id' => 64,
+                    'op_type_id' => 63,
                     'stn_id' => $transaction['stn_id'],
                     'cash_col' => $transaction['cash_col'],
                     'cash_ret' => $transaction['cash_ret'],
@@ -2160,7 +2170,7 @@ class ClAccounting extends Controller
                         'cl_acc_id' => $clAccId,
                         'txn_date' => $transaction['txn_date'],
                         'engraved_id' => $transaction['engraved_id'],
-                        'pen_type_id' => 34,
+                        'pen_type_id' => 33,
                         'pen_price' => $transaction['total_penalty'],
                         'stn_id' => $transaction['stn_id'],
                         'media_type_id' => $transaction['media_type_id'],
@@ -2200,26 +2210,26 @@ class ClAccounting extends Controller
             try {
 
                 DB::table('cl_tp_accounting')->insert([
-                    'atek_id' => $transaction['atek_id'],
-                    'txn_date' => $transaction['txn_date'],
-                    'engraved_id' => $transaction['engraved_id'],
-                    'op_type_id' => 64,
-                    'stn_id' => $transaction['stn_id'],
-                    'cash_col' => $transaction['cash_col'],
-                    'cash_ret' => $transaction['cash_ret'],
-                    'pass_price' => $transaction['pass_price'],
-                    'card_fee' => $transaction['card_fee'],
-                    'card_sec' => $transaction['card_sec'],
-                    'processing_fee' => $transaction['processing_fee'],
-                    'total_price' => $transaction['total_price'],
-                    'pass_ref_chr' => $transaction['pass_ref_chr'],
-                    'card_fee_ref_chr' => $transaction['card_fee_ref_chr'],
-                    'card_sec_ref_chr' => $transaction['card_sec_ref_chr'],
-                    'num_trips' => $transaction['num_trips'],
-                    'rem_trips' => $transaction['rem_trips'],
-                    'media_type_id' => $transaction['media_type_id'],
-                    'product_id' => $transaction['product_id'],
-                    'pass_id' => $transaction['pass_id'],
+                    'atek_id'           => $transaction['atek_id'],
+                    'txn_date'          => $transaction['txn_date'],
+                    'engraved_id'       => $transaction['engraved_id'],
+                    'op_type_id'        => 64,
+                    'stn_id'            => $transaction['stn_id'],
+                    'cash_col'          => $transaction['cash_col'],
+                    'cash_ret'          => $transaction['cash_ret'],
+                    'pass_price'        => $transaction['pass_price'],
+                    'card_fee'          => $transaction['card_fee'],
+                    'card_sec'          => $transaction['card_sec'],
+                    'processing_fee'    => $transaction['processing_fee'],
+                    'total_price'       => $transaction['total_price'],
+                    'pass_ref_chr'      => $transaction['pass_ref_chr'],
+                    'card_fee_ref_chr'  => $transaction['card_fee_ref_chr'],
+                    'card_sec_ref_chr'  => $transaction['card_sec_ref_chr'],
+                    'num_trips'         => $transaction['num_trips'],
+                    'rem_trips'         => $transaction['rem_trips'],
+                    'media_type_id'     => $transaction['media_type_id'],
+                    'product_id'        => $transaction['product_id'],
+                    'pass_id'           => $transaction['pass_id'],
                     'pass_expiry' => $transaction['pass_expiry'],
                     'src_stn_id' => $transaction['src_stn_id'],
                     'des_stn_id' => $transaction['des_stn_id'],
@@ -2284,7 +2294,7 @@ class ClAccounting extends Controller
 
     }/* OP_TYPE_ID = 64*/
 
-    public function exitMismatchNoEntry($transaction)
+    public function exitMismatchNoExit($transaction)
     {
 
         /* CHECK THAT IS THESE ATTRIBUTES ARE NULLABLE OR NOT */
@@ -2390,26 +2400,26 @@ class ClAccounting extends Controller
             try {
 
                 DB::table('cl_tp_accounting')->insert([
-                    'atek_id'           => $transaction['atek_id'],
-                    'txn_date'          => $transaction['txn_date'],
-                    'engraved_id'       => $transaction['engraved_id'],
-                    'op_type_id'        => 65,
-                    'stn_id'            => $transaction['stn_id'],
-                    'cash_col'          => $transaction['cash_col'],
-                    'cash_ret'          => $transaction['cash_ret'],
-                    'pass_price'        => $transaction['pass_price'],
-                    'card_fee'          => $transaction['card_fee'],
-                    'card_sec'          => $transaction['card_sec'],
-                    'processing_fee'    => $transaction['processing_fee'],
-                    'total_price'       => $transaction['total_price'],
-                    'pass_ref_chr'      => $transaction['pass_ref_chr'],
-                    'card_fee_ref_chr'  => $transaction['card_fee_ref_chr'],
-                    'card_sec_ref_chr'  => $transaction['card_sec_ref_chr'],
-                    'num_trips'         => $transaction['num_trips'],
-                    'rem_trips'         => $transaction['rem_trips'],
-                    'media_type_id'     => $transaction['media_type_id'],
-                    'product_id'        => $transaction['product_id'],
-                    'pass_id'           => $transaction['pass_id'],
+                    'atek_id' => $transaction['atek_id'],
+                    'txn_date' => $transaction['txn_date'],
+                    'engraved_id' => $transaction['engraved_id'],
+                    'op_type_id' => 65,
+                    'stn_id' => $transaction['stn_id'],
+                    'cash_col' => $transaction['cash_col'],
+                    'cash_ret' => $transaction['cash_ret'],
+                    'pass_price' => $transaction['pass_price'],
+                    'card_fee' => $transaction['card_fee'],
+                    'card_sec' => $transaction['card_sec'],
+                    'processing_fee' => $transaction['processing_fee'],
+                    'total_price' => $transaction['total_price'],
+                    'pass_ref_chr' => $transaction['pass_ref_chr'],
+                    'card_fee_ref_chr' => $transaction['card_fee_ref_chr'],
+                    'card_sec_ref_chr' => $transaction['card_sec_ref_chr'],
+                    'num_trips' => $transaction['num_trips'],
+                    'rem_trips' => $transaction['rem_trips'],
+                    'media_type_id' => $transaction['media_type_id'],
+                    'product_id' => $transaction['product_id'],
+                    'pass_id' => $transaction['pass_id'],
                     'pass_expiry' => $transaction['pass_expiry'],
                     'src_stn_id' => $transaction['src_stn_id'],
                     'des_stn_id' => $transaction['des_stn_id'],
@@ -2472,7 +2482,197 @@ class ClAccounting extends Controller
         $transData['atek_id'] = $transaction['atek_id'];
         return $transData;
 
-
     }/* OP_TYPE_ID = 65*/
+
+    public function exitMismatchNoEntry($transaction)
+    {
+
+        /* CHECK THAT IS THESE ATTRIBUTES ARE NULLABLE OR NOT */
+        $paxFirstName = "";
+        $paxLastName = "";
+        $paxMobile = 123456789;
+        $paxGenType = 0;
+
+        if (array_key_exists("pax_first_name", $transaction)) $paxFirstName = $transaction['pax_first_name'];
+        if (array_key_exists("pax_last_name", $transaction)) $paxLastName = $transaction['pax_last_name'];
+        if (array_key_exists("pax_mobile", $transaction)) $paxMobile = $transaction['pax_mobile'];
+        if (array_key_exists("pax_gen_type", $transaction)) $paxGenType = $transaction['pax_gen_type'];
+
+        if ($transaction['product_id'] == 3) {
+
+            DB::beginTransaction();
+
+            try {
+
+                DB::table('cl_sv_accounting')->insert([
+                    'atek_id' => $transaction['atek_id'],
+                    'txn_date' => $transaction['txn_date'],
+                    'engraved_id' => $transaction['engraved_id'],
+                    'op_type_id' => 66,
+                    'stn_id' => $transaction['stn_id'],
+                    'cash_col' => $transaction['cash_col'],
+                    'cash_ret' => $transaction['cash_ret'],
+                    'pass_price' => $transaction['pass_price'],
+                    'card_fee' => $transaction['card_fee'],
+                    'card_sec' => $transaction['card_sec'],
+                    'processing_fee' => $transaction['processing_fee'],
+                    'total_price' => $transaction['total_price'],
+                    'pass_ref_chr' => $transaction['pass_ref_chr'],
+                    'card_fee_ref_chr' => $transaction['card_fee_ref_chr'],
+                    'card_sec_ref_chr' => $transaction['card_sec_ref_chr'],
+                    'pre_chip_bal' => $transaction['pre_chip_bal'],
+                    'pos_chip_bal' => $transaction['pos_chip_bal'],
+                    'media_type_id' => $transaction['media_type_id'],
+                    'product_id' => $transaction['product_id'],
+                    'pass_id' => $transaction['pass_id'],
+                    'pass_expiry' => $transaction['pass_expiry'],
+                    'pax_first_name' => $paxFirstName,
+                    'pax_last_name' => $paxLastName,
+                    'pax_mobile' => $paxMobile,
+                    'pax_gen_type' => $paxGenType,
+                    'shift_id' => $transaction['shift_id'],
+                    'user_id' => $transaction['user_id'],
+                    'eq_id' => $transaction['eq_id'],
+                    'pay_type_id' => $transaction['pay_type_id'],
+                    'pay_ref' => $transaction['pay_ref'],
+                    'is_test' => $transaction['is_test'],
+                    'old_engraved_id' => $transaction['old_engraved_id'],
+                ]);
+
+                $clAccId = DB::table('cl_sv_accounting')
+                    ->where('atek_id', '=', $transaction['atek_id'])
+                    ->select('cl_acc_id')
+                    ->value('cl_acc_id');
+
+                /* PEN TYPE ID 14 */
+                DB::table('cl_pen_accounting')
+                    ->insert([
+                        'cl_acc_id' => $clAccId,
+                        'txn_date' => $transaction['txn_date'],
+                        'engraved_id' => $transaction['engraved_id'],
+                        'pen_type_id' => 36,
+                        'pen_price' => $transaction['total_penalty'],
+                        'stn_id' => $transaction['stn_id'],
+                        'media_type_id' => $transaction['media_type_id'],
+                        'product_id' => $transaction['product_id'],
+                        'pass_id' => $transaction['pass_id'],
+                    ]);
+
+            } catch (PDOException $e) {
+
+                /* IF COLUMN IDENTITY FOUND AS ERROR */
+                if ($e->getCode() == 23505) { /* 23505 IS ERROR CODE FROM POSTGRESQL */
+                    $transData['is_settled'] = true;
+                } else {
+                    $transData['is_settled'] = false;
+                }
+                $transData['atek_id'] = $transaction['atek_id'];
+                $transData['error'] = $e->getMessage();
+
+                DB::rollBack();
+
+                return $transData;
+
+            }
+
+            DB::commit();
+
+            $transData['is_settled'] = true;
+            $transData['atek_id'] = $transaction['atek_id'];
+            return $transData;
+
+        }
+
+        if ($transaction['product_id'] == 4) {
+
+            //DB::beginTransaction();
+
+            try {
+
+                DB::table('cl_tp_accounting')->insert([
+                    'atek_id'           => $transaction['atek_id'],
+                    'txn_date'          => $transaction['txn_date'],
+                    'engraved_id'       => $transaction['engraved_id'],
+                    'op_type_id'        => 66,
+                    'stn_id'            => $transaction['stn_id'],
+                    'cash_col'          => $transaction['cash_col'],
+                    'cash_ret'          => $transaction['cash_ret'],
+                    'pass_price'        => $transaction['pass_price'],
+                    'card_fee'          => $transaction['card_fee'],
+                    'card_sec'          => $transaction['card_sec'],
+                    'processing_fee'    => $transaction['processing_fee'],
+                    'total_price'       => $transaction['total_price'],
+                    'pass_ref_chr'      => $transaction['pass_ref_chr'],
+                    'card_fee_ref_chr'  => $transaction['card_fee_ref_chr'],
+                    'card_sec_ref_chr'  => $transaction['card_sec_ref_chr'],
+                    'num_trips'         => $transaction['num_trips'],
+                    'rem_trips'         => $transaction['rem_trips'],
+                    'media_type_id'     => $transaction['media_type_id'],
+                    'product_id'        => $transaction['product_id'],
+                    'pass_id'           => $transaction['pass_id'],
+                    'pass_expiry'       => $transaction['pass_expiry'],
+                    'src_stn_id'        => $transaction['src_stn_id'],
+                    'des_stn_id'        => $transaction['des_stn_id'],
+                    'pax_first_name'    => $paxFirstName,
+                    'pax_last_name'     => $paxLastName,
+                    'pax_mobile'        => $paxMobile,
+                    'pax_gen_type'      => $paxGenType,
+                    'shift_id'          => $transaction['shift_id'],
+                    'user_id'           => $transaction['user_id'],
+                    'eq_id'             => $transaction['eq_id'],
+                    'pay_type_id'       => $transaction['pay_type_id'],
+                    'pay_ref'           => $transaction['pay_ref'],
+                    'is_test'           => $transaction['is_test'],
+                    'old_engraved_id'   => $transaction['old_engraved_id'],
+                ]);
+
+                $clAccId = DB::table('cl_tp_accounting')
+                    ->where('atek_id', '=', $transaction['atek_id'])
+                    ->select('cl_acc_id')
+                    ->value('cl_acc_id');
+
+                /* PEN TYPE ID 31 */
+                DB::table('cl_pen_accounting')
+                    ->insert([
+                        'cl_acc_id' => $clAccId,
+                        'txn_date' => $transaction['txn_date'],
+                        'engraved_id' => $transaction['engraved_id'],
+                        'pen_type_id' => 36,
+                        'pen_price' => $transaction['total_penalty'],
+                        'stn_id' => $transaction['stn_id'],
+                        'media_type_id' => $transaction['media_type_id'],
+                        'product_id' => $transaction['product_id'],
+                        'pass_id' => $transaction['pass_id'],
+                    ]);
+
+            } catch (PDOException $e) {
+                /* IF COLUMN IDENTITY FOUND AS ERROR */
+                if ($e->getCode() == 23505) { /* 23505 IS ERROR CODE FROM POSTGRESQL */
+                    $transData['is_settled'] = true;
+                } else {
+                    $transData['is_settled'] = false;
+                }
+                $transData['atek_id'] = $transaction['atek_id'];
+                $transData['error'] = $e->getMessage();
+
+                DB::rollBack();
+
+                return $transData;
+            }
+
+            DB::commit();
+
+            $transData['is_settled'] = true;
+            $transData['atek_id'] = $transaction['atek_id'];
+            return $transData;
+
+        }
+
+        $transData['error'] = "Product Not Found !";
+        $transData['atek_id'] = $transaction['atek_id'];
+        return $transData;
+
+
+    }/* OP_TYPE_ID = 66*/
 
 }
